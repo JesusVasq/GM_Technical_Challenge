c19fc19a10cb44e94e652ede27303622bda3820c
Jesus Vasquez <jesus.i.vasq@gmail.com>
2021-02-10 23:33:05 -0600

    parsed out fields from JSON data returned

--- app/src/main/java/com/jesusvasquez/gmtechnical/MainActivity.java
+++ app/src/main/java/com/jesusvasquez/gmtechnical/MainActivity.java
@@ -10,56 +10,57 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.*;
 
 public class MainActivity extends AppCompatActivity {
 
     final static String TAG = "GM_Challenge";
-    final static String PUBLIC_REPO = "https://api.github.com/repos/twbs/bootstrap/commits";
+    final static String PUBLIC_REPO = "https://api.github.com/repos/JesusVasq/GM_Technical_Challenge/commits";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fetchCommitHistory(PUBLIC_REPO);
+        fetchCommits();
     }
-    public void fetchCommitHistory(String url){
-        Log.d(TAG,"getting user info");
-        RequestQueue queue = Volley.newRequestQueue(this);
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        ((TextView)findViewById(R.id.hello)).setText("Response succeeded");
-                        Log.d(TAG,"call succeeded: " + response);
-                        try {
-                            JSONArray myArray = new JSONArray((response));
-                            JSONObject first = myArray.getJSONObject(0);
-                            String SHA = first.getString("sha");
-                            Log.d(TAG, "SHA is: " + SHA);
-                        }catch (JSONException err){
-                            Log.d(TAG, "Error: "+ err.toString());
-                        }
+    public void fetchCommits() {
+        Log.d(TAG,"Fetching commits...");
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, PUBLIC_REPO, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    for(int i = 0; i < response.length(); i++){
+                        JSONObject commitObject = response.getJSONObject(i);
+                        JSONObject commitInfo = commitObject.getJSONObject("commit");
+                        JSONObject authorInfo = commitInfo.getJSONObject("author");
+
+                        String commitHash = commitObject.getString("sha");
+                        String authorName = authorInfo.getString("name");
+                        String message = commitInfo.getString("message");
+
+                        Log.d(TAG,"hash: " + commitHash);
+                        Log.d(TAG,"authorName: " + authorName);
+                        Log.d(TAG,"message: " + message);
+
                     }
-                }, new Response.ErrorListener() {
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.d(TAG,"API call succeeded!");
+            }
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                ((TextView)findViewById(R.id.hello)).setText("That didn't work!");
-                Log.d(TAG,"call didnt succeed");
+                Log.d(TAG,"API call failed!");
             }
         });
-        queue.add(stringRequest);
-    }
 
-    public void parseCommitJSON(String data){
-
+        requestQueue.add(jsonArrayRequest);
     }
 }
